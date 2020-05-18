bef516e289b3ac606928755cfd856a87c373608f
Michael Lucas <michiqt@live.de>
2020-05-18 13:41:54 +0200

    Auslagerungen

--- ueb02/src/logic.c
+++ ueb02/src/logic.c
@@ -15,16 +15,16 @@
 
 Game game = {0, 1, 1, GAME_RUNNING};
 
-// TODO: Aendern
+// TODO: Aendern + P_START entfernen und dafuer Startkoordinaten einfuegen
 Levels levels[3] = {{
                             {2, {
                                         {P_WALL, P_WALL, P_WALL, P_WALL, P_WALL, P_WALL, P_WALL, P_WALL, P_WALL},
-                                        {P_WALL, P_START, P_BOX, P_BOX, P_OBJECT_TRIANGLE, P_FREE, P_DOOR, P_DOOR_SWITCH, P_WALL},
+                                        {P_WALL, P_START, P_FREE, P_FREE, P_FREE, P_FREE, P_DOOR, P_DOOR_SWITCH, P_WALL},
                                         {P_WALL, P_FREE, P_HOUSE, P_BOX, P_FREE, P_FREE, P_BOX, P_FREE, P_WALL},
                                         {P_WALL, P_FREE, P_BOX, P_WALL, P_FREE, P_BOX, P_FREE, P_FREE, P_WALL},
                                         {P_WALL, P_BOX, P_FREE, P_BOX, P_FREE, P_BOX, P_FREE, P_FREE, P_WALL},
                                         {P_WALL, P_FREE, P_BOX, P_FREE, P_BOX, P_FREE, P_FREE, P_FREE, P_WALL},
-                                        {P_WALL, P_BOX, P_FREE, P_BOX, P_FREE, P_BOX, P_FREE, P_PORTAL, P_WALL},
+                                        {P_WALL, P_BOX, P_FREE, P_BOX, P_PORTAL, P_BOX, P_FREE, P_OBJECT_TRIANGLE, P_WALL},
                                         {P_WALL, P_PORTAL, P_BOX, P_FREE, P_BOX, P_BOX, P_FREE, P_TARGET, P_WALL},
                                         {P_WALL, P_WALL, P_WALL, P_WALL, P_WALL, P_WALL, P_WALL, P_WALL, P_WALL}}},
 
@@ -55,32 +55,36 @@ pushyFieldType getBlockOfPos(int x, int y) {
     return levels[game.levelId]->field[y][x];
 }
 
-int moveBox(enum e_Direction direction, int x, int y) {
-    int newX = x;
-    int newY = y;
-
-    //todo auslagern?
-    switch (direction) {
-        case dirLeft:
-            newX = x - 1;
-            break;
-        case dirRight:
-            newX = x + 1;
-            break;
-        case dirUp:
-            newY = y - 1;
-            break;
-        case dirDown:
-            newY = y + 1;
-            break;
+/**
+ * Neue Position fuer x oder y bestimmen
+ * @param val - x oder y Wert
+ * @param direction - Wenn richtung z.B nach oben ist, muss der Y Wert verringert werden
+ * @param isX - true, wenn der X-Wert bearbeitet werden soll
+ * @return
+ */
+int newPos(int val, enum e_Direction direction, GLboolean isX) {
+    if ((direction == dirLeft && isX) || (direction == dirUp && !isX)) {
+        val -= 1;
+    } else if ((direction == dirRight && isX) || (direction == dirDown && !isX)) {
+        val += 1;
     }
 
-    if (y > 0
-        && (getBlockOfPos(newX, newY) == P_FREE
-            || getBlockOfPos(newX, newY) == P_DOOR_SWITCH)) {
+    return val;
+}
+
+int moveObject(enum e_Direction direction, int x, int y, pushyFieldType fieldType) {
+    int newX = newPos(x, direction, GL_TRUE);
+    int newY = newPos(y, direction, GL_FALSE);
+    pushyFieldType blockOfPos = getBlockOfPos(newX, newY);
+
+    if (blockOfPos == P_FREE || (blockOfPos == P_DOOR_SWITCH && fieldType == P_BOX) ||
+        (blockOfPos == P_TARGET && fieldType == P_OBJECT_TRIANGLE)) {
         //TODO P_BOX_DOOR_SWITCH einbauen
+
+        if (fieldType != P_OBJECT_TRIANGLE) {
+            levels[game.levelId]->field[newY][newX] = fieldType;
+        }
         levels[game.levelId]->field[y][x] = P_FREE;
-        levels[game.levelId]->field[newY][newX] = P_BOX;
         return GL_TRUE;
     }
     return GL_FALSE;
@@ -89,46 +93,38 @@ int moveBox(enum e_Direction direction, int x, int y) {
 GLboolean playerMovementAllowed(enum e_Direction direction) {
     int pX = game.playerPosX;
     int pY = game.playerPosY;
-    int pXNew = pX;
-    int pYNew = pY;
 
-    //todo auslagern?
-    switch (direction) {
-        case dirLeft:
-            pXNew = pX - 1;
-            break;
-        case dirRight:
-            pXNew = pX + 1;
-            break;
-        case dirUp:
-            pYNew = pY - 1;
-            break;
-        case dirDown:
-            pYNew = pY + 1;
-            break;
-    }
+    int pXNew = newPos(pX, direction, GL_TRUE);
+    int pYNew = newPos(pY, direction, GL_FALSE);
     int checkTile;
 
-    if(direction == dirUp && pY >= 1
-    || direction == dirDown && pY < LEVEL_SIZE
-    ||direction == dirLeft && pX >= 1
-    ||direction == dirRight && pX < LEVEL_SIZE ){
+    if ((direction == dirUp && pY >= 1)
+        || (direction == dirDown && pY < LEVEL_SIZE)
+        || (direction == dirLeft && pX >= 1)
+        || (direction == dirRight && pX < LEVEL_SIZE)) {
 
         checkTile = getBlockOfPos(pXNew, pYNew);
 
-        if (checkTile == P_BOX) {
-            return moveBox(direction, pXNew, pYNew);
-        } else if (checkTile == P_OBJECT_TRIANGLE) {
+        switch (checkTile) {
+            case P_BOX:
+            case P_OBJECT_TRIANGLE:
+                return moveObject(direction, pXNew, pYNew, checkTile);
 
-        } else if (checkTile == P_HOUSE) {
+            case P_HOUSE:
+                break;
 
-        } else if (checkTile == P_WALL) {
-            return GL_FALSE;
+            case P_FREE:
+                return GL_TRUE;
+
+            case P_WALL:
+            case P_TARGET:
+            default:
+                return GL_FALSE;
+
         }
-        return GL_TRUE;
     }
-    return GL_FALSE;
 
+    return GL_FALSE;
 }
 
 /**
