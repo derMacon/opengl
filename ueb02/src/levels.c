ff462b5a8eae99c33dcd700113ce0699eab17e58
Marcel Hippler <mhippler@posteo.de>
2020-05-21 17:46:20 +0200

    neues level und level gefixt

--- ueb02/src/levels.c
+++ ueb02/src/levels.c
@@ -23,27 +23,6 @@ const Level levels[3] = {{
                          },
                          {
                                  .field = {
-                                         {P_WALL, P_WALL,  P_WALL, P_WALL, P_WALL, P_WALL, P_WALL,   P_WALL,   P_WALL},
-                                         {P_WALL, P_START, P_FREE, P_FREE, P_FREE, P_BOX,  P_DOOR,   P_BOX,    P_WALL},
-                                         {P_WALL, P_BOX,   P_FREE, P_FREE, P_FREE, P_FREE, P_BOX,    P_FREE,   P_WALL},
-                                         {P_WALL, P_FREE,  P_BOX,  P_FREE, P_FREE, P_BOX,  P_FREE,   P_FREE,   P_WALL},
-                                         {P_WALL, P_BOX,   P_FREE, P_BOX,  P_FREE, P_BOX,  P_PORTAL, P_PORTAL, P_WALL},
-                                         {P_WALL, P_FREE,  P_BOX,  P_FREE, P_BOX,  P_BOX,  P_FREE,   P_FREE,   P_WALL},
-                                         {P_WALL, P_BOX,   P_FREE, P_BOX,  P_FREE, P_BOX,  P_FREE,   P_PORTAL, P_WALL},
-                                         {P_WALL, P_FREE,  P_BOX,  P_FREE, P_BOX,  P_BOX,  P_PORTAL, P_TARGET, P_WALL},
-                                         {P_WALL, P_WALL,  P_WALL, P_WALL, P_WALL, P_WALL, P_WALL,   P_WALL,   P_WALL}},
-                                 .startPos = {4, 5},
-                                 .time = 27,
-                                 .doors = {{-1, -1},
-                                           {-1, -1},
-                                           {-1, -1}},
-                                 .doorSwitch = {{-1, -1},
-                                                {-1, -1},
-                                                {-1, -1}}
-
-                         },
-                         {
-                                 .field = {
                                          {P_OUTER, P_WALL, P_WALL,        P_WALL,            P_WALL, P_WALL,            P_WALL,   P_WALL,   P_OUTER},
                                          {P_OUTER, P_WALL, P_DOOR_SWITCH, P_HOUSE,           P_BOX,  P_FREE,            P_FREE,   P_WALL,   P_OUTER},
                                          {P_OUTER, P_WALL, P_FREE,        P_FREE,            P_BOX,  P_FREE,            P_FREE,   P_WALL,   P_OUTER},
@@ -54,13 +33,34 @@ const Level levels[3] = {{
                                          {P_OUTER, P_WALL, P_BOX,         P_BOX,             P_FREE, P_FREE,            P_PORTAL, P_WALL,   P_OUTER},
                                          {P_OUTER, P_WALL, P_WALL,        P_WALL,            P_WALL, P_WALL,            P_WALL,   P_WALL,   P_OUTER}
                                  },
-                                 .startPos = {3, 3},
+                                 .startPos = {4, 5},
                                  .time = 30,
-                                 .doors = {{-1, -1},
+                                 .doors = {{2, 6},
                                            {-1, -1},
                                            {-1, -1}},
-                                 .doorSwitch = {{-1, -1},
+                                 .doorSwitch = {{2, 1},
                                                 {-1, -1},
                                                 {-1, -1}}
+                         },
+                         {
+                                 .field = {
+                                         {P_WALL,  P_WALL,  P_WALL, P_WALL,            P_WALL, P_WALL,        P_WALL,            P_WALL,        P_WALL},
+                                         {P_WALL,  P_FREE,  P_FREE, P_FREE,            P_BOX,  P_DOOR_SWITCH, P_DOOR_SWITCH,     P_DOOR_SWITCH, P_WALL},
+                                         {P_WALL,  P_FREE,  P_FREE, P_OBJECT_TRIANGLE, P_BOX,  P_FREE,        P_OBJECT_TRIANGLE, P_FREE,        P_WALL},
+                                         {P_WALL,  P_FREE,  P_FREE, P_FREE,            P_BOX,  P_FREE,        P_OBJECT_TRIANGLE, P_FREE,        P_WALL},
+                                         {P_WALL,  P_FREE,  P_FREE, P_OBJECT_TRIANGLE, P_BOX,  P_FREE,        P_OBJECT_TRIANGLE, P_FREE,        P_WALL},
+                                         {P_WALL,  P_FREE,  P_FREE, P_FREE,            P_BOX,  P_FREE,        P_FREE,            P_TARGET,      P_WALL},
+                                         {P_WALL,  P_WALL,  P_FREE, P_FREE,            P_WALL, P_WALL,        P_WALL,            P_WALL,        P_WALL},
+                                         {P_OUTER, P_WALL,  P_WALL, P_FREE,            P_DOOR, P_DOOR,        P_DOOR,            P_HOUSE,       P_WALL},
+                                         {P_OUTER, P_OUTER, P_WALL, P_WALL,            P_WALL, P_WALL,        P_WALL,            P_WALL,        P_WALL}},
+                                 .startPos = {1, 1},
+                                 .time = 120,
+                                 .doors = {{4, 7},
+                                           {5, 7},
+                                           {6, 7}},
+                                 .doorSwitch = {{5, 1},
+                                                {6, 1},
+                                                {7, 1}}
+
                          }
 };
