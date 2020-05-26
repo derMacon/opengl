cae2013dfe06a753937899b5d471f08f2d098bf9
Michael Lucas <michiqt@live.de>
2020-05-26 18:10:46 +0200

    Kommentare + refactor

--- ueb02/src/types.h
+++ ueb02/src/types.h
@@ -14,20 +14,16 @@
 GLboolean showWireframe;
 GLboolean showFullscreen;
 
-/*
- * Art von Levelfeldern
- */
+/** Art von Levelfeldern */
 typedef enum {
     P_OUTER, P_FREE, P_WALL, P_BOX, P_TARGET, P_OBJECT_TRIANGLE,
     P_DOOR, P_DOOR_SWITCH, P_BOX_DOOR_SWITCH, P_PORTAL, P_HOUSE, P_PLAYER
 } pushyFieldType;
 
-/* Spielfeld */
+/** Spielfeld */
 typedef pushyFieldType pushyLevel[LEVEL_SIZE][LEVEL_SIZE];
 
-/* Zeiger auf ein Spielfeld */
-typedef pushyFieldType (*pushyLevelPointer)[LEVEL_SIZE];
-
+/** Spielstatus  */
 typedef enum {
     GAME_RUNNING,
     GAME_WON,
@@ -40,28 +36,34 @@ typedef enum {
 enum e_Direction {
     dirLeft, dirRight, dirUp, dirDown, dirNone
 };
-
+/** Leveleinstellungen des Spiels */
 typedef struct {
-    int portal1PosX, portal1PosY, portal2PosX, portal2PosY;
-    int playerPosX, playerPosY;
-    int numberOfTriangles;
-    int time;
-    int numberOfDoors;
-    pushyLevel level;
+    int
+            portal1PosX, portal1PosY, portal2PosX, portal2PosY,
+            playerPosX, playerPosY,
+            numberOfTriangles,
+            numberOfDoors,
+            time;
+    pushyLevel
+            level;
 } LevelSettings;
 
+/** Spieleinstellungen */
 typedef struct {
     int levelId;
     Gamestatus gameStatus;
     LevelSettings levelSettings;
 } Game;
 
+/** Level des Spiels */
 typedef struct {
-    int time;
-    int startPos[2];
-    pushyLevel field;
-    int doors[MAXIMUM_NUMBER_OF_DOORS][2];
-    int doorSwitch[MAXIMUM_NUMBER_OF_DOORS][2];
+    int
+            time,
+            startPos[2],
+            doors[MAXIMUM_NUMBER_OF_DOORS][2],
+            doorSwitch[MAXIMUM_NUMBER_OF_DOORS][2];
+    pushyLevel
+            field;
 } Level;
 
 #endif //UEB01_TYPES_H
